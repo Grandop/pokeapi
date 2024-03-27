48a340de6ddb9cde27959915240daeb9b0acc2fd
Pedro <pedro.grando@voritgo.com.br>
2024-03-26 23:12:26 -0300

    doc: configure jest

---
+++ src/components/Divider/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { Divider } from ".";
+
+describe("<Divider />", () => {
+  it("should render Divider", () => {
+    render(<Divider />);
+  });
+});
